e2d68df8975593a878e6ac5e16a7fe3012c2c3cc
Heliezer Garcia <helifacul@gmail.com>
2021-04-24 23:24:40 -0300

    Captura de dados no Banco
    Como Capturar dados no banco através do ID.

--- src/main/java/dominio/App.java
+++ src/main/java/dominio/App.java
@@ -9,18 +9,32 @@ public class App
 {
     public static void main( String[] args )
     {
+        /* 
+        Exemplo para instanciar classes em caso de inserção de dados;
         Pessoa p1 = new Pessoa(null, "Maria dos Santos", "maria@gmail.com");
         Pessoa p2 = new Pessoa(null, "Heliezer Garcia Gonçalves", "helie@gmail.com");
-        Pessoa p3 = new Pessoa(null, "Santos Dumont", "dumont@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Santos Dumont", "dumont@gmail.com"); */
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
+
+       /*  Para inserir os dados utilize as linhas abaixo 
 
         em.getTransaction().begin();
         em.persist(p1);
         em.persist(p2);
         em.persist(p3);
-        em.getTransaction().commit();
+        em.getTransaction().commit(); 
+
+        */
+
+        // Exemplo para buscar dados pelo ID;
+        Pessoa p = em.find(Pessoa.class, 8);
+
+
+        System.out.println(p);
+        
+        
 
         System.out.println("Insert Data OK");
         emf.close();
